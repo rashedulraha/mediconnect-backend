4c8b4368ef829008f1f9abd82e44d48874d0d77f
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-08-24 12:16:30 +0600

    refactor(auth): update auth interfaces for registration, login, and change password payloads

--- src/app/module/auth/auth.interface.ts
+++ src/app/module/auth/auth.interface.ts
@@ -1,23 +1,30 @@
-import { Role } from "../../../generated/prisma/browser";
-
-export interface ILoginUserPayload {
-  email: string;
-  password: string;
-}
+import { Role } from "../../../generated/prisma/enums";
 
 export interface IRegisterPatientPayload {
-  name: string;
-  email: string;
-  password: string;
+	name: string;
+	email: string;
+	password: string;
+	contactNumber?: string;
+	address?: string;
+	patient?: {
+		contactNumber?: string;
+		address?: string;
+	};
+}
+
+export interface ILoginUserPayload {
+	email: string;
+	password: string;
 }
 
 export interface IRequestUser {
-  userId: string;
-  email: string;
-  name: string;
-  role: Role;
+	userId: string;
+	email: string;
+	name: string;
+	role: Role;
 }
 
-export interface IGoogleLoginPayload {
-  idToken: string;
+export interface IChangePasswordPayload {
+	currentPassword: string;
+	newPassword: string;
 }
