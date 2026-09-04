b3ef4acd7ce5bb139cc468c9972b0b6280b10c6b
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-09-04 16:03:32 +0600

    style(auth): format validation contracts

--- src/app/module/auth/auth.interface.ts
+++ src/app/module/auth/auth.interface.ts
@@ -1,40 +1,40 @@
 import { Role } from "../../../generated/prisma/enums";
 
 export interface IRegisterPatientPayload {
-  name: string;
-  email: string;
-  password: string;
-  contactNumber?: string;
-  address?: string;
-  patient?: {
-    contactNumber?: string;
-    address?: string;
-  };
+	name: string;
+	email: string;
+	password: string;
+	contactNumber?: string;
+	address?: string;
+	patient?: {
+		contactNumber?: string;
+		address?: string;
+	};
 }
 
 export interface ILoginUserPayload {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 export interface IRequestUser {
-  userId: string;
-  email: string;
-  name: string;
-  role: Role;
+	userId: string;
+	email: string;
+	name: string;
+	role: Role;
 }
 
 export interface IChangePasswordPayload {
-  currentPassword: string;
-  newPassword: string;
+	currentPassword: string;
+	newPassword: string;
 }
 
 export interface IForgotPassword {
-  email: string;
+	email: string;
 }
 
 export interface IResetPassword {
-  email: string;
-  newPassword: string;
-  otp: string;
+	email: string;
+	newPassword: string;
+	otp: string;
 }
